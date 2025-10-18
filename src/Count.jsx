35974f8d49afbccae4879b34bebb13765f5f3a7b
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-10-18 20:37:03 +0530

    first-commit

--- src/Count.jsx
+++ src/Count.jsx
@@ -9,7 +9,7 @@ let Count = () => {
 
   return (
     <div className="bg-gray-950 w-full text-white h-screen flex justify-center items-center flex-col gap-10">
-      <h1 className="text-4xl">Count: {likeCount}</h1>
+      <h1 className="text-4xl">Like Count: {likeCount}</h1>
       <div className="flex justify-between items-center w-60">
         <LikeButtun likeCount={likeCount} setlikeCount={setlikeCount} /> 
         <DisLikeButtun likeCount={likeCount} setlikeCount={setlikeCount}/>
